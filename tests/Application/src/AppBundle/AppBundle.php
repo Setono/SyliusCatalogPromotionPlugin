fbc458324ae26cf500da1d153ad616c91b4a5dc0
Igor Mukhin <igor.mukhin@gmail.com>
2018-09-28 03:00:18 +0300

    Fixed: Mappings for AppBundle models

--- tests/Application/src/AppBundle/AppBundle.php
+++ tests/Application/src/AppBundle/AppBundle.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace AppBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class AppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver(
+            array(
+                realpath(__DIR__ . '/Resources/config/doctrine/model') => 'AppBundle\Model',
+            ),
+            array('doctrine.orm.entity_manager')
+        ));
+    }
 }
