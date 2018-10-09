f2eb9effa22dacecde00671cdf4e459785d9b9b8
Igor Mukhin <igor.mukhin@gmail.com>
2018-10-09 22:07:03 +0300

    Added: Validators-related stuff

---
+++ src/Validator/Constraints/SpecialDateRange.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusBulkSpecialsPlugin\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+final class SpecialDateRange extends Constraint
+{
+    /**
+     * @var string
+     */
+    public $message = 'setono_sylius_bulk_specials.special.end_date_cannot_be_set_prior_start_date';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets(): string
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validatedBy(): string
+    {
+        return 'setono_sylius_bulk_specials_special_date_range_validator';
+    }
+}
---
+++ src/Validator/SpecialDateRangeValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusBulkSpecialsPlugin\Validator;
+
+use Setono\SyliusBulkSpecialsPlugin\Model\SpecialInterface;
+use Setono\SyliusBulkSpecialsPlugin\Validator\Constraints\SpecialDateRange;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Webmozart\Assert\Assert;
+
+final class SpecialDateRangeValidator extends ConstraintValidator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value, Constraint $constraint): void
+    {
+        if (null === $value) {
+            return;
+        }
+
+        /** @var SpecialInterface $value */
+        Assert::isInstanceOf($value, SpecialInterface::class);
+
+        /** @var SpecialDateRange $constraint */
+        Assert::isInstanceOf($constraint, SpecialDateRange::class);
+
+        if (null === $value->getStartsAt() || null === $value->getEndsAt()) {
+            return;
+        }
+
+        if ($value->getStartsAt()->getTimestamp() > $value->getEndsAt()->getTimestamp()) {
+            $this->context
+                ->buildViolation($constraint->message)
+                ->atPath('endsAt')
+                ->addViolation()
+            ;
+        }
+    }
+}
