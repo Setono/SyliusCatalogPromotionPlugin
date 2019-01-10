ad0180722664cb8d7b8c0d0012eb8ef56638ab67
Igor Mukhin <igor.mukhin@gmail.com>
2019-01-10 22:04:07 +0200

    Fixed: Issue when select was empty on Special update

--- src/Resources/public/js/app.js
+++ src/Resources/public/js/app.js
@@ -1,6 +1,10 @@
 (function($) {
     $(document).ready(function () {
 
+        setTimeout(function(){
+          $('select[name^="setono_sylius_bulk_specials_special[rules]"][name$="[type]"]').change();
+        }, 50);
+
         $('#rules a[data-form-collection="add"]').on('click', function () {
             setTimeout(function(){
                 $('select[name^="setono_sylius_bulk_specials_special[rules]"][name$="[type]"]').last().change();
