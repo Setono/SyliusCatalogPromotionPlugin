11aaa55f4f3a097551c943fb321234485a30f2ce
Joachim Løvgaard <joachim@loevgaard.dk>
2019-12-02 14:54:52 +0100

    changed the timeout

--- src/Resources/public/js/app.js
+++ src/Resources/public/js/app.js
@@ -7,7 +7,7 @@
         $('#rules a[data-form-collection="add"]').on('click', function () {
             setTimeout(function(){
                 $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').last().change();
-            }, 50);
+            }, 200);
         });
     });
 })(jQuery);
