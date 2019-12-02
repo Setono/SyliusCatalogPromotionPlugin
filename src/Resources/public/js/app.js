ee759b30279943a5a10b1128e606a8ef37db7abf
Joachim Løvgaard <joachim@loevgaard.dk>
2019-12-02 15:27:45 +0100

    Changing timeout didn't work ._.

--- src/Resources/public/js/app.js
+++ src/Resources/public/js/app.js
@@ -2,12 +2,12 @@
     $(function () {
         setTimeout(function(){
           $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').change();
-        }, 2000);
+        }, 50);
 
         $('#rules a[data-form-collection="add"]').on('click', function () {
             setTimeout(function(){
                 $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').last().change();
-            }, 2000);
+            }, 50);
         });
     });
 })(jQuery);
