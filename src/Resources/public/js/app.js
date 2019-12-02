67dd389397d103b475e34934912bd3c28e32a45e
Joachim Løvgaard <joachim@loevgaard.dk>
2019-12-02 15:49:22 +0100

    running js and non-js scenarios separately

--- src/Resources/public/js/app.js
+++ src/Resources/public/js/app.js
@@ -1,13 +1,13 @@
-(function($) {
-    $(function () {
-        setTimeout(function(){
-          $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').change();
-        }, 50);
+(function ($) {
+  $(function () {
+    setTimeout(function () {
+      $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').change();
+    }, 50);
 
-        $('#rules a[data-form-collection="add"]').on('click', function () {
-            setTimeout(function(){
-                $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').last().change();
-            }, 50);
-        });
+    $('#rules a[data-form-collection="add"]').on('click', function () {
+      setTimeout(function () {
+        $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').last().change();
+      }, 50);
     });
+  });
 })(jQuery);
