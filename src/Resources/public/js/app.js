46ca071d9f62fe2b470613c1fbd626a01ea37573
Joachim Løvgaard <joachim@loevgaard.dk>
2019-12-02 15:15:52 +0100

    Trying to increase the timeout very much

--- src/Resources/public/js/app.js
+++ src/Resources/public/js/app.js
@@ -2,12 +2,12 @@
     $(function () {
         setTimeout(function(){
           $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').change();
-        }, 50);
+        }, 2000);
 
         $('#rules a[data-form-collection="add"]').on('click', function () {
             setTimeout(function(){
                 $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').last().change();
-            }, 200);
+            }, 2000);
         });
     });
 })(jQuery);
