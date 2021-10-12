86d6d144f7efb2c7766865062c5d2a68d514cb07
Igor Mukhin <igor.mukhin@gmail.com>
2021-10-12 16:32:39 +0300

    Fixed: Implementation of has_taxon/has_not_taxon rules to work correctly together

--- src/Rule/HasTaxonRule.php
+++ src/Rule/HasTaxonRule.php
@@ -6,11 +6,22 @@ namespace Setono\SyliusCatalogPromotionPlugin\Rule;
 
 use Doctrine\ORM\QueryBuilder;
 use function sprintf;
+use Sylius\Component\Core\Model\ProductTaxon;
+use Sylius\Component\Core\Model\ProductVariant;
+use Sylius\Component\Core\Model\TaxonInterface;
+use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
 use Webmozart\Assert\Assert;
 
 final class HasTaxonRule extends Rule
 {
     public const TYPE = 'has_taxon';
+
+    private TaxonRepositoryInterface $taxonRepository;
+
+    public function __construct(TaxonRepositoryInterface $taxonRepository)
+    {
+        $this->taxonRepository = $taxonRepository;
+    }
 
     public function filter(QueryBuilder $queryBuilder, array $configuration): void
     {
@@ -18,18 +29,29 @@ final class HasTaxonRule extends Rule
         Assert::isArray($value);
 
         $rootAlias = $this->getRootAlias($queryBuilder);
-        $productAlias = $this->join($queryBuilder, sprintf('%s.product', $rootAlias), 'product');
-        $productTaxonsAlias = $this->join($queryBuilder, sprintf('%s.productTaxons', $productAlias), 'product_taxons');
-        $taxonAlias = $this->join($queryBuilder, sprintf('%s.taxon', $productTaxonsAlias), 'taxon');
-        $parameter = self::generateParameter('taxon_codes');
+        $productVariantAlias = self::generateAlias('pv');
+        $productTaxonAlias = self::generateAlias('pt');
+        $parameter = self::generateParameter('include_taxons');
+
+        $taxons = array_map(function (string $taxonCode): TaxonInterface {
+            /** @var TaxonInterface|null $taxon */
+            $taxon = $this->taxonRepository->findOneBy(['code' => $taxonCode]);
+            Assert::notNull($taxon);
+
+            return $taxon;
+        }, $value);
+
+        $em = $queryBuilder->getEntityManager();
+        $subQueryBuilder = $em->createQuery(
+            sprintf('SELECT %s.id ', $productVariantAlias) .
+            sprintf('FROM %s AS %s ', ProductVariant::class, $productVariantAlias) .
+            sprintf('LEFT JOIN %s AS %s WITH %s.product=%s.product ', ProductTaxon::class, $productTaxonAlias, $productTaxonAlias, $productVariantAlias) .
+            sprintf('WHERE %s.taxon IN (:%s)', $productTaxonAlias, $parameter)
+        );
 
         $queryBuilder
-            ->andWhere(sprintf(
-                '%s.code IN (:%s)',
-                $taxonAlias,
-                $parameter
-            ))
-            ->setParameter($parameter, $value)
+            ->andWhere(sprintf('%s.id IN (%s)', $rootAlias, $subQueryBuilder->getDQL()))
+            ->setParameter($parameter, $taxons)
         ;
     }
 }
